a02a108fb1fa4c5dc8ca775ceae9dee5e56e227c
wangce <wangce_email@163.com>
2020-12-03 14:59:37 +0800

    update

--- src/components/videoPlay.vue
+++ src/components/videoPlay.vue
@@ -1,45 +1,21 @@
 <template>
   <div class="videoWrapper">
-    <video
-      id="myVideo"
-      class="video-js vjs-big-play-centered "
-      controls
-      preload="auto"
-      width="164"
-      height="218"
-      data-setup='{}'
-    >
-      <source
-        src="https://yzl-pub-test-oss.youbao.wang/video/20201202/A73fF8_1606895493554.mp4"
-        type="video/mp4"
-      >
-    </video>
   </div>
 </template>
 <script>
-import videojs from 'video.js'
-import 'video.js/dist/video-js.css'
 export default {
+  data() {
+    return {
+    }
+  },
   mounted() {
-    videojs('myVideo', {
-      controlBar: false,
-      playbackRates: [0.5, 1, 1.5, 2],
-      ControlBar: {
-        customControlSpacer: true
-      }
-    },
-      function onPlayerReady() {
-        console.log('1111111')
-      })
+
+  },
+  methods: {
+    play() {
+    }
   }
 }
 </script>
 <style lang="less" scoped>
-.videoWrapper{
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  }
 </style>
